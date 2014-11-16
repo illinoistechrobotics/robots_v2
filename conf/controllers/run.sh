03869b1671607d3c79a38a8dd7064177678859b1
Adrian Birylo <abirylo@gmail.com>
2014-11-15 23:10:45 -0500

    fixed shell script for linux

--- conf/controllers/run.sh
+++ conf/controllers/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Djava.ext.dirs=lib/JInput:lib/RXTX -jar "controllers.jar"
+java -Dgnu.io.rxtx.SerialPorts=/dev/ttyACM0:/dev/ttyACM1:/dev/ttyACM2:/dev/ttyACM3 -Djava.ext.dirs=lib/JInput:lib/RXTX -jar "controllers.jar"
