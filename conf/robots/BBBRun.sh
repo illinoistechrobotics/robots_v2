f01e7ca9537422fa3fae55ba85931a0ef0ba8856
abirylo <abirylo@gmail.com>
2015-03-03 21:10:02 -0500

    Small Changes to BBBRun Script.

--- conf/robots/BBBRun.sh
+++ conf/robots/BBBRun.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 #Need to list the ACM* type Serial Ports since RXTX doesn't auto detect them
-java -Dgnu.io.rxtx.SerialPorts=/dev/ttyO0:/dev/ttyO1:/dev/ttyO2:/dev/ttyO3:/dev/ttyO5 -Djava.ext.dirs=lib:lib/JInput/BBB:lib/RXTX/BBB:/lib/Bulldog/Debian_Ubuntu -jar "robots.jar" -r Modulus -sp /dev/ttyO2 -sb 9600 -j "0 Logitech Logitech Dual Action"
-
+/home/robot/jdk1.7.0_60/bin/java -Dgnu.io.rxtx.SerialPorts=/dev/ttyO0:/dev/ttyO1:/dev/ttyO2:/dev/ttyO3:/dev/ttyO5 -Djava.ext.dirs=lib:lib/JInput/BBB:lib/RXTX/BBB:lib/BullDog/Debian_Ubuntu -jar "robots.jar" -r Modulus -ip 192.168.1.105 -esp 5500 -ecp 5600 -j "0 Logitech Logitech Dual Action"
--- src/org/illinoistechrobotics/robot/Modulus.java
+++ src/org/illinoistechrobotics/robot/Modulus.java
@@ -19,7 +19,7 @@ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
 LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+ */
 package org.illinoistechrobotics.robot;
 
 import org.bulldog.beagleboneblack.BBBNames;
@@ -35,48 +35,46 @@ import org.illinoistechrobotics.common.RobotEnum;
 import org.illinoistechrobotics.common.Timer;
 import org.illinoistechrobotics.jaguar.Jaguar;
 
-public class Modulus extends EventManager{
-	
+public class Modulus extends EventManager {
+
 	private Board board;
 	private DigitalOutput output;
 	private Jaguar jaguar;
-	
-	public Modulus(Queue q, Communication c, Timer t){
-		super(q,c,t,RobotEnum.MODULUS);
+
+	public Modulus(Queue q, Communication c, Timer t) {
+		super(q, c, t, RobotEnum.MODULUS);
 	}
-	
+
 	@Override
-	public void on_init(){
+	public void on_init() {
 		super.on_init();
 		board = Platform.createBoard();
 		output = board.getPin(BBBNames.P8_12).as(DigitalOutput.class);
 		output.write(Signal.High);
 		timer.timer1hz = true;
-		
+
 		String port = System.getProperty("jaguar.port", "/dev/ttyO0");
 		jaguar = new Jaguar();
-		jaguar.open("port");
+		jaguar.open(port);
 	}
-	
+
 	@Override
-	public void on_failsafe(){
-		
+	public void on_failsafe() {
+
 	}
-	
+
 	@Override
-    public void on_axis_change(Event ev){
+	public void on_axis_change(Event ev) {
 		super.on_axis_change(ev);
 		System.out.println(ev.toString());
-		
+
 	}
-	
+
 	@Override
-    public void on_1hz_timer(Event ev){
+	public void on_1hz_timer(Event ev) {
 		super.on_1hz_timer(ev);
 		output.toggle();
 		System.out.println("1hz");
 	}
-	
-	
-	
+
 }
